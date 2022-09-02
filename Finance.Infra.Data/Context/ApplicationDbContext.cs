0b9efb1ec93aa5e8157dce0b077a9f677781f715
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-09-02 17:54:03 -0300

    Criado classe Dbcontext

--- Finance.Infra.Data/Context/ApplicationDbContext.cs
+++ Finance.Infra.Data/Context/ApplicationDbContext.cs
@@ -4,10 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Finance.Domain.Entities;
 
 namespace Finance.Infra.Data.Context
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        //public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Conta> Contas {get; set;}
+        public DbSet<PlanoConta> PlanosContas {get; set;}
+        public DbSet<Transacao> Transacoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
     }
 }
